d7eaadaa904f72e7619db71467c3d4474ded8bdf
Zain Shk <zshk599@gmail.com>
2025-08-10 19:12:18 +0500

    redesign two section

--- src/components/sections/SectionFour.tsx
+++ src/components/sections/SectionFour.tsx
@@ -2,10 +2,47 @@ import Image from "next/image";
 import React from "react";
 
 export default function SectionFour() {
+  const stats = [
+    { label: "UI/UX, веб-дизайн", value: 10, color: "bg-purple-300" },
+    {
+      label: "Логотипы, брендинг, фирстиль",
+      value: 38,
+      color: "bg-purple-200",
+    },
+    { label: "3D и Моушн-дизайн", value: 40, color: "bg-purple-200" },
+    { label: "Презентации и визуализация", value: 60, color: "bg-purple-200" },
+    { label: "Иллюстрации и Art", value: 47, color: "bg-purple-200" },
+    { label: "Прочий digital-дизайн", value: 65, color: "bg-purple-200" },
+  ];
+ const stats2 = [
+    { label: "UI/UX, веб-дизайн", value: 10, color: "bg-green-300" },
+    {
+      label: "Логотипы, брендинг, фирстиль",
+      value: 38,
+      color: "bg-green-200",
+    },
+    { label: "3D и Моушн-дизайн", value: 40, color: "bg-green-200" },
+    { label: "Презентации и визуализация", value: 60, color: "bg-green-200" },
+    { label: "Иллюстрации и Art", value: 47, color: "bg-green-200" },
+    { label: "Прочий digital-дизайн", value: 65, color: "bg-green-200" },
+  ];  
+
+  const stats3 = [
+    { label: "UI/UX, веб-дизайн", value: 10, color: "bg-blue-300" },
+    {
+      label: "Логотипы, брендинг, фирстиль",
+      value: 38,
+      color: "bg-blue-200",
+    },
+    { label: "3D и Моушн-дизайн", value: 40, color: "bg-blue-200" },
+    { label: "Презентации и визуализация", value: 60, color: "bg-blue-200" },
+    { label: "Иллюстрации и Art", value: 47, color: "bg-blue-200" },
+    { label: "Прочий digital-дизайн", value: 65, color: "bg-blue-200" },
+  ];  
   return (
     <section className="px-7 max-w-[1650px] 2xl:mx-auto md:px-14 pb-16 lg:pb-32 ">
       {/* desktop */}
-      <div className="hidden lg:flex flex-col">
+      <div className="flex flex-col">
         <div className="font-lato text-center lg:max-w-3xl mx-auto mb-5  space-y-4">
           <h1 className="text-[48px] font-bold leading-[52px]">
             Посмотрите, на какие задачи уже нанимают специалистов
@@ -16,36 +53,163 @@ export default function SectionFour() {
           </p>
         </div>
 
-        <div className="flex flex-col gap-5 w-full justify-center items-center">
-          <Image
-            src="/sect4.png"
-            alt="chart"
-            width={850}
-            height={500}
-            quality={100}
-            className="h-full w-full"
-          />{" "}
-          <Image
-            src="/sect4b.png"
-            alt="chart"
-            width={850}
-            height={500}
-            quality={100}
-            className="h-full w-full"
-          />{" "}
-          <Image
-            src="/sect4c.png"
-            alt="chart"
-            width={850}
-            height={500}
-            quality={100}
-            className="h-full w-full"
-          />{" "}
+        <div className="bg-[#F9F9FA] p-5 lg:p-8 rounded-2xl w-full my-5  mx-auto">
+          <div className="flex font-lato relative flex-col lg:flex-row lg:items-start gap-6">
+            {/* Left Side */}
+            <div className=" lg:w-1/2 flex lg:flex-col flex-row-reverse  h-full ">
+              {/* Top percentage */}
+              <div className="flex lg:flex-col items-center lg:items-start justify-between mb-4 lg:mb-0">
+                <span className="text-[22px] lg:text-[64px] leading-[32px] lg:leading-[100%] font-semibold lg:font-normal">
+                  49%
+                </span>
+              </div>
+
+              {/* Title & Description */}
+              <div className="lg:absolute lg:w-1/2 lg:bottom-0">
+                <h2 className="text-[22px] lg:text-[32px] font-semibold leading-[32px] lg:leading-[40px] mb-2">
+                  Дизайн
+                </h2>
+                <p className="text-[16px] leading-[24px] lg:leading-[28px] lg:text-[18px] font-normal text-gray-600">
+                  Каждый второй на платформе — дизайнер. Большинство делают
+                  интерфейсы и сайты
+                </p>
+              </div>
+            </div>
+
+            {/* Right Side */}
+            <div className="flex flex-col lg:w-1/2 gap-3 w-full">
+              {stats.map((item, index) => (
+                <div key={index} className="w-full">
+                  <div className="flex justify-between gap-3 items-center text-sm">
+                    {/* Progress Bar with text */}
+                    <div className="relative w-full h-10 bg-purple-100 rounded-lg overflow-hidden">
+                      {/* Filled bar */}
+                      <div
+                        className={`h-full rounded-r-xl ${item.color}`}
+                        style={{ width: `${item.value}%` }}
+                      ></div>
+
+                      {/* Text always fully visible */}
+                      <span className="absolute inset-y-0 left-2 flex items-center text-black whitespace-nowrap">
+                        {item.label}
+                      </span>
+                    </div>
+                    <span>{item.value}%</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Mobile Description */}
+          {/* <div className="lg:hidden mt-4">
+            <h2 className="text-lg font-semibold mb-2">Дизайн</h2>
+            <p className="text-sm text-gray-600">
+              Каждый второй на платформе — дизайнер. Большинство делают
+              интерфейсы и сайты
+            </p>
+          </div> */}
+        </div>
+        <div className="bg-[#F9F9FA] p-5 lg:p-8 rounded-2xl w-full my-5  mx-auto">
+          <div className="flex font-lato relative flex-col lg:flex-row-reverse lg:items-start gap-6">
+            {/* Left Side */}
+            <div className=" lg:w-1/2 flex lg:flex-col flex-row-reverse  h-full ">
+              {/* Top percentage */}
+              <div className="flex lg:flex-col items-center lg:items-start justify-between mb-4 lg:mb-0">
+                <span className="text-[22px] lg:text-[64px] leading-[32px] lg:leading-[100%] font-semibold lg:font-normal">
+                  49%
+                </span>
+              </div>
+
+              {/* Title & Description */}
+              <div className="lg:absolute lg:w-1/2 lg:bottom-0">
+                <h2 className="text-[22px] lg:text-[32px] font-semibold leading-[32px] lg:leading-[40px] mb-2">
+                  Дизайн
+                </h2>
+                <p className="text-[16px] leading-[24px] lg:leading-[28px] lg:text-[18px] font-normal text-gray-600">
+                  Каждый второй на платформе — дизайнер. Большинство делают
+                  интерфейсы и сайты
+                </p>
+              </div>
+            </div>
+
+            {/* Right Side */}
+            <div className="flex flex-col lg:w-1/2 gap-3 w-full">
+              {stats2.map((item, index) => (
+                <div key={index} className="w-full">
+                  <div className="flex justify-between gap-3 items-center text-sm">
+                    {/* Progress Bar with text */}
+                    <div className="relative w-full h-10 bg-green-100 rounded-lg overflow-hidden">
+                      {/* Filled bar */}
+                      <div
+                        className={`h-full rounded-r-xl ${item.color}`}
+                        style={{ width: `${item.value}%` }}
+                      ></div>
+
+                      {/* Text always fully visible */}
+                      <span className="absolute inset-y-0 left-2 flex items-center text-black whitespace-nowrap">
+                        {item.label}
+                      </span>
+                    </div>
+                    <span>{item.value}%</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#F9F9FA] p-5 lg:p-8 rounded-2xl w-full lg:my-5  mx-auto">
+          <div className="flex font-lato relative flex-col lg:flex-row lg:items-start gap-6">
+            {/* Left Side */}
+            <div className=" lg:w-1/2 flex lg:flex-col flex-row-reverse  h-full ">
+              {/* Top percentage */}
+              <div className="flex lg:flex-col items-center lg:items-start justify-between mb-4 lg:mb-0">
+                <span className="text-[22px] lg:text-[64px] leading-[32px] lg:leading-[100%] font-semibold lg:font-normal">
+                  49%
+                </span>
+              </div>
+
+              {/* Title & Description */}
+              <div className="lg:absolute lg:w-1/2 lg:bottom-0">
+                <h2 className="text-[22px] lg:text-[32px] font-semibold leading-[32px] lg:leading-[40px] mb-2">
+                  Дизайн
+                </h2>
+                <p className="text-[16px] leading-[24px] lg:leading-[28px] lg:text-[18px] font-normal text-gray-600">
+                  Каждый второй на платформе — дизайнер. Большинство делают
+                  интерфейсы и сайты
+                </p>
+              </div>
+            </div>
+
+            {/* Right Side */}
+            <div className="flex flex-col lg:w-1/2 gap-3 w-full">
+              {stats3.map((item, index) => (
+                <div key={index} className="w-full">
+                  <div className="flex justify-between gap-3 items-center text-sm">
+                    {/* Progress Bar with text */}
+                    <div className="relative w-full h-10 bg-blue-100 rounded-lg overflow-hidden">
+                      {/* Filled bar */}
+                      <div
+                        className={`h-full rounded-r-xl ${item.color}`}
+                        style={{ width: `${item.value}%` }}
+                      ></div>
+
+                      {/* Text always fully visible */}
+                      <span className="absolute inset-y-0 left-2 flex items-center text-black whitespace-nowrap">
+                        {item.label}
+                      </span>
+                    </div>
+                    <span>{item.value}%</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
       {/* mobile */}
-      <div className="lg:hidden mt-10">
+      {/* <div className="lg:hidden mt-10">
         <div className="font-lato text-left max-w-xs mb-5 space-y-4">
           <h1 className="text-[28px] font-semibold leading-[34px]">
             Посмотрите, на какие задачи уже нанимают специалистов
@@ -65,24 +229,8 @@ export default function SectionFour() {
             quality={100}
             className="h-full w-full"
           />{" "}
-          {/* <Image
-            src="/sect4b.png"
-            alt="chart"
-            width={550}
-            height={500}
-            quality={100}
-            className="h-full w-full"
-          />{" "}
-          <Image
-            src="/sect4c.png"
-            alt="chart"
-            width={550}
-            height={500}
-            quality={100}
-            className="h-full w-full"
-          />{" "} */}
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
--- src/components/sections/SectiontThree.tsx
+++ src/components/sections/SectiontThree.tsx
@@ -4,9 +4,9 @@ import React from "react";
 export default function SectiontThree() {
   return (
     <section className="px-7 max-w-[1600px] mx-auto md:px-14 lg:px-30 pb-16 lg:pb-32 lg:py-20 ">
-      <div className="hidden lg:grid grid-cols-1">
-        <div className="flex flex-col lg:flex-row justify-between gap-6 py-5 items-center ">
-          <div className="w-full md:max-w-xl lg:max-w-3xl relative">
+      <div className="grid grid-cols-1">
+        <div className="flex flex-col lg:flex-row justify-between gap-5 py-5 items-center ">
+          <div className="w-full md:max-w-xl lg:max-w-3xl lg:mr-10 relative">
             <h1 className="font-lato text-[32px] sm:text-[36px] md:text-[42px] lg:text-[48px] font-bold leading-[52px] text-black">
               Собрали главное о специалистах на Relevants — в цифрах
               и диаграммах
@@ -16,47 +16,151 @@ export default function SectiontThree() {
               направления самые активные и с кем чаще всего стоит
               сотрудничество.
             </p>
-            <div className="absolute hidden lg:flex -bottom-6 right-3 " >
-              <Image
-                src="/vector325.svg"
-                alt="arrow"
-                width={80}
-                height={80}
-              />
+            <div className="absolute hidden lg:flex -bottom-6 right-3 ">
+              <Image src="/vector325.svg" alt="arrow" width={80} height={80} />
             </div>
           </div>
-          <div className="w-full">
-            <Image
-              src="/donut1.png"
-              alt="donut"
-              width={650}
-              height={200}
-              className="w-full h-full"
-            />
+          <div className="bg-[#F9F9FA] p-5 lg:p-8 rounded-2xl w-full mx-auto">
+            {/* Title */}
+            <h1 className="lg:text-[32px] text-xl font-semibold leading-[40px] mb-6">
+              По направлениям
+            </h1>
+
+            {/* Content */}
+            <div className="flex flex-col lg:flex-row items-center gap-6">
+              {/* Donut Image */}
+              <div className="flex justify-center flex-shrink-0">
+                <Image
+                  src="/donut1.png"
+                  alt="donut"
+                  width={200}
+                  height={200}
+                  className="w-40 h-40 lg:w-48 lg:h-48 object-contain"
+                />
+              </div>
+
+              {/* Text + Ratio */}
+              <div className="flex flex-col justify-center w-full gap-3">
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-black"></span>
+                    Дизайн
+                  </span>
+                  <span>49%</span>
+                </div>
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-blue-300"></span>
+                    Маркетинг
+                  </span>
+                  <span>24%</span>
+                </div>
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-green-200"></span>
+                    IT Разработка
+                  </span>
+                  <span>19%</span>
+                </div>
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-purple-200"></span>
+                    Видеомонтаж, CGIи GameDev
+                  </span>
+                  <span>19%</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex flex-col lg:flex-row justify-between gap-6 py-5 items-center ">
-          <div className="w-full md:max-wxl">
-            <Image
-              src="/donut1.png"
-              alt="donut"
-              width={650}
-              height={200}
-              className="w-full h-full"
-            />
+          <div className="bg-[#F9F9FA] p-5 lg:p-8 rounded-2xl w-full max-w-3xl mx-auto">
+            {/* Title */}
+            <h1 className="lg:text-[32px] text-xl font-semibold leading-[40px] mb-6">
+              По опыту
+            </h1>
+
+            {/* Content */}
+            <div className="flex flex-col lg:flex-row items-center gap-6">
+              {/* Donut Image */}
+              <div className="flex justify-center flex-shrink-0">
+                <Image
+                  src="/donut1.png"
+                  alt="donut"
+                  width={200}
+                  height={200}
+                  className="w-40 h-40 lg:w-48 lg:h-48 object-contain"
+                />
+              </div>
+
+              {/* Text + Ratio */}
+              <div className="flex flex-col justify-center w-full gap-3">
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-black"></span>
+                    Junior
+                  </span>
+                  <span>29%</span>
+                </div>
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-blue-300"></span>
+                    Middle
+                  </span>
+                  <span>52%</span>
+                </div>
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-green-200"></span>
+                    Senior
+                  </span>
+                  <span>19%</span>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="w-full md:max--2xl">
-            <Image
-              src="/donut1.png"
-              alt="donut"
-              width={650}
-              height={200}
-              className="w-full h-full"
-            />
+          <div className="bg-[#F9F9FA] p-5 lg:p-8 rounded-2xl w-full max-w-3xl mx-auto">
+            {/* Title */}
+            <h1 className="lg:text-[32px] text-xl font-semibold leading-[40px] mb-6">
+              По формату работы
+            </h1>
+
+            {/* Content */}
+            <div className="flex flex-col lg:flex-row items-center gap-6">
+              {/* Donut Image */}
+              <div className="flex justify-center flex-shrink-0">
+                <Image
+                  src="/donut1.png"
+                  alt="donut"
+                  width={200}
+                  height={200}
+                  className="w-40 h-40 lg:w-48 lg:h-48 object-contain"
+                />
+              </div>
+
+              {/* Text + Ratio */}
+              <div className="flex flex-col justify-center w-full gap-3">
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-black"></span>
+                    Проект / парттайм
+                  </span>
+                  <span>63%</span>
+                </div>
+                <div className="flex justify-between text-base">
+                  <span className="flex items-center gap-2">
+                    <span className="w-3 h-3 rounded-full bg-blue-300"></span>
+                    Фултайм /найм
+                  </span>
+                  <span>37%</span>
+                </div>
+                
+              </div>
+            </div>
           </div>
         </div>
       </div>
-       <div className="lg:hidden mt-10 grid grid-cols-1 lg:place-items-center">
+      {/* <div className="lg:hidden mt-10 grid grid-cols-1 lg:place-items-center">
         <div className="flex flex-col lg:flex-row justify-between gap-6 items-center ">
           <div className="w-full sm:max-w-xl lg:max-w-[645px] relative">
             <h1 className="font-lato text-[28px] font-semibold leading-[34px] text-black">
@@ -68,7 +172,6 @@ export default function SectiontThree() {
               направления самые активные и с кем чаще всего стоит
               сотрудничество.
             </p>
-            
           </div>
           <div className="w-full md:max-w-2xl">
             <Image
@@ -100,7 +203,7 @@ export default function SectiontThree() {
             />
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
